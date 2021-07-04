4212ce9e9de315c297635e30f5d21c23583ffef8
SammyRF <rong.fan@live.de>
2021-07-04 06:50:37 +0200

    remake docker file

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
-FROM python:3.6-slim-stretch
+FROM ubuntu
 
-RUN pip install pandas xlwt
-
-VOLUME [ "/data" ]
-
-COPY ./conv.py .
-COPY ./*.tar .
-
-CMD ["python", "conv.py"]
+ARG DEBIAN_FRONTEND=noninteractive
+RUN apt-get update
+RUN apt-get install -y python
+RUN apt-get install -y nodejs
+RUN apt-get install -y npm
+RUN npm i -g groq-cli
--- conv.py
+++ conv.py
@@ -1,31 +1,30 @@
-import json
-import pandas
 import os
 import zipfile
-import sys
 import shutil
+import json
 
 def convert(tar_file):
     print(tar_file)
     temp = './temp'
-    excel_file = '/data/' + tar_file[:-4] + '.xls'
     zipfile.ZipFile(tar_file).extractall(temp)
-    excel_writer = pandas.ExcelWriter(excel_file)
+    logs = []
     for subdir, _, files in os.walk(temp):
         for filename in files:
             if filename.endswith('.log'):
+                name = subdir[-10:]
                 filepath = subdir + os.sep + filename
                 with open(filepath) as f:
-                    lines = f.readlines()
-                    data = [json.loads(line) for line in lines]
-                    cols = list(json.loads(lines[0]).keys())
-                    pandas.DataFrame(data, columns=cols).to_excel(excel_writer, sheet_name=subdir[-8:])
-    excel_writer.save()
+                    for line in f.readlines():
+                        logs.append('{"file":"' + name + '",' + line[1:])
     shutil.rmtree(temp)
+
+    with open("result.json", "w") as outfile: 
+        json.dump(json.loads('[' + ','.join(logs) + ']'), outfile, indent=4)
 
 if __name__ == '__main__':
     for _, _, files in os.walk('./'):
         for filename in files:
             if filename.endswith('.tar'):
                 convert(filename)
+    
 
